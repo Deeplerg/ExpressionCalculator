fa12806b380d763d91804197f3413f5e31efd377
Deeplerg <deeplerg@gmail.com>
2023-12-26 23:49:06 +0500

    Add parsing priority to FactorialTokenParser

--- src/ExpressionCalculator/Tokenization/Parsers/Simple/FactorialTokenParser.cs
+++ src/ExpressionCalculator/Tokenization/Parsers/Simple/FactorialTokenParser.cs
@@ -3,6 +3,7 @@ using ExpressionCalculator.Tokenization.Tokens.Operators;
 
 namespace ExpressionCalculator.Tokenization.Parsers.Simple;
 
+[ParsingPriority(1_110_000)]
 public class FactorialTokenParser : MatchingTokenParserBase<FactorialToken>
 {
     public FactorialTokenParser() : base("!")
